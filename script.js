38e5a5cbc8888c154a410934d6fbda3e8e160540
Iustin Pericica <ipericica@microsoft.com>
2023-11-22 01:48:39 +0200

    Update HTML title and add JavaScript functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+
+// Get the button element
+const changeColorButton = document.getElementById('changeColorButton');
+
+// Add a click event listener to the button
+changeColorButton.addEventListener('click', () => {
+    // Generate a random color
+    const randomColor = getRandomColor();
+
+    // Change the background color of the body
+    document.body.style.backgroundColor = randomColor;
+});
+
+// Function to generate a random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
