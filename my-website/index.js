4ffeef70fa199499958ad7f1418b0e65b3b94601
iustinpericica <32682995+iustinpericica@users.noreply.github.com>
2023-11-22 01:05:24 +0000

    azure works

---
+++ my-website/index.js
@@ -0,0 +1,64 @@
+const questionElement = document.getElementById('question');
+const optionsElement = document.getElementById('options');
+const nextButton = document.getElementById('next-btn');
+const resultElement = document.getElementById('result');
+
+let currentQuestionIndex = 0;
+let questions;
+
+// Fetch questions from the JSON file
+fetch('questions.json')
+    .then(response => response.json())
+    .then(data => {
+        questions = data.questions;
+        displayQuestion();
+    })
+    .catch(error => console.error('Error fetching questions:', error));
+
+function displayQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    questionElement.innerText = currentQuestion.question;
+
+    optionsElement.innerHTML = '';
+    currentQuestion.options.forEach((option, index) => {
+        const button = document.createElement('button');
+        button.innerText = option;
+        button.classList.add('option');
+        button.onclick = () => checkAnswer(index);
+        optionsElement.appendChild(button);
+    });
+}
+
+function checkAnswer(userIndexAnswer) {
+    const correctAnswer = questions[currentQuestionIndex].correctAnswer;
+    if (userIndexAnswer === correctAnswer) {
+        resultElement.innerText = 'Correct!';
+        createConfetti();
+    } else {
+        resultElement.innerText = 'Incorrect!';
+    }
+}
+
+function nextQuestion() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion();
+        resultElement.innerText = '';
+    } else {
+        // Show final score
+        resultElement.innerText = 'Quiz completed!';
+        nextButton.style.display = 'none';
+        optionsElement.innerHTML = '';
+        questionElement.innerText = '';
+    }
+}
+
+function createConfetti() {
+    const scalar = 3;
+    const pineapple = confetti.shapeFromText({ text: '💕', scalar, gravity: 0.3 });
+
+    confetti({
+    shapes: [pineapple, 'circle'],
+    scalar,
+    });
+} 
