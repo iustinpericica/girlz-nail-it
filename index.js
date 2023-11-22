644bbbe2bc41195bc1edda4eabccdc30dbde573e
Iustin Pericica <ipericica@microsoft.com>
2023-11-22 12:54:16 +0200

    withAi

--- index.js
+++ index.js
@@ -7,13 +7,58 @@ let currentQuestionIndex = 0;
 let questions;
 
 // Fetch questions from the JSON file
-fetch('questions.json')
-    .then(response => response.json())
-    .then(data => {
-        questions = data.questions;
-        displayQuestion();
+// fetch('questions.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         questions = data.questions;
+//         displayQuestion();
+//     })
+//     .catch(error => console.error('Error fetching questions:', error));
+
+document.getElementById('loader').style.display = 'block';
+document.getElementById('next-btn').style.display = 'none';
+
+fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+        "content-type": "application/json",
+        "Authorization": 'Bearer ' + 'xxx'
+    },
+    body: JSON.stringify({
+        "model": "gpt-4-1106-preview",
+        "response_format": {
+            "type": "json_object"
+        },
+        "messages": [
+            {
+                "role": "system",
+                "content": "Return the output as JSON for questions for a user based on the details provided by user...... Don't use \n in the json and respond with an API like json."
+            },
+            {
+                "role": "user",
+                "content": "Generate an array with questions from biology. The structure of each question must be: {question: string, options: array<string>, correctAnswer: integer} the correctAnswer property is the correct option index on the options array. The response must be in json format."
+            }
+        ],
+        "temperature": 0.7
     })
-    .catch(error => console.error('Error fetching questions:', error));
+}).then(response => {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('next-btn').style.display = 'inline-block';
+
+    return response.json().then(data => {
+        return JSON.parse(data.choices[0].message.content);
+    })
+}).then(openAiData => {
+    questions = openAiData.questions;
+    console.log(questions);
+    displayQuestion();
+}).catch(err => {
+    console.error('Error fetching data:', error);
+    // Hide the loader even if there's an error
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('next-btn').style.display = 'inline-block';
+
+});
 
 function displayQuestion() {
     const currentQuestion = questions[currentQuestionIndex];
